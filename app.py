4e18a795a4e3d16053acd761d198940de772b740
Shubhendu Halder <141364632+MRWICKJ@users.noreply.github.com>
2024-10-27 12:21:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,6 @@ import nltk
 from nltk.stem.porter import PorterStemmer
 
 
-# Download specific datasets without using the GUI
-nltk.download('punkt')      # For the Punkt tokenizer
-nltk.download('stopwords')  # For stopwords
-nltk.download('punkt_tab')
-
 ps = PorterStemmer()
 
 
